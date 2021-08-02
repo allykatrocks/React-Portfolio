a62f1d10bd063035e01b14cf2eabb3687ebbfd0e
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-08-02 16:22:51 -0700

    beginning of contact functionality

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const Contact = () => {
-    return (<div>Contact</div>)
-}
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log("state changed", formState);
+    }
+  };
+
+  const submitChange = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      console.log("submit worked", formState);
+    }
+  };
+
+  return (
+    <section>
+      <form id="contact-form" onSubmit={submitChange}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={formState.name}
+            onBlur={handleChange}
+          />
+        </div>
+        <div></div>
+        <div></div>
+        {errorMessage && (
+          <div>
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        <button type="submit">submit</button>
+      </form>
+    </section>
+  );
+};
 
 export default Contact;
