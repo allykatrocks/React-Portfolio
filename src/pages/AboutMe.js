a38079a0b933f91854a91c1019eb787f9c90a92f
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-08-02 16:52:33 -0700

    about me content

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const AboutMe = () => {
-    return (<div>About Me</div>)
+    return (<section class="about-me" id="about-me">
+    <h2>About Me</h2>
+    <p>
+      Hello! My name is Alex and I'm a junior web developer. I am a graduate
+      of San Diego State University with a Bachelor's degree in Theater
+      Arts. I began my coding journey in December of 2020 in order to gain
+      profitable and relevant skills I can take with me to any career field.
+      Please find below a collection of my completed and deployed works as
+      well as how to contact me.
+    </p>
+  </section>)
 }
 
 export default AboutMe;
