3e9a6baa49e21f162ca09883d0e110783d8699ff
Alex DeMarco <demarcoalexandria5@gmail.com>
2021-08-04 19:03:23 -0700

    added footer

--- src/App.js
+++ src/App.js
@@ -23,39 +23,66 @@ function App() {
       <header>
         <h1>Alex DeMarco</h1>
         {/* <div> */}
-          <ul className="navbar">
-            <li className={currentPage === 'About Me' ? "active": ""}
-              onClick={() => {
-                setCurrentPage("About Me");
-              }}
-            >
-              About Me
-            </li>
-            <li className={currentPage === 'Contact' ? "active": ""}
-              onClick={() => {
-                setCurrentPage("Contact");
-              }}
-            >
-              Contact
-            </li>
-            <li className={currentPage === 'Portfolio' ? "active": ""}
-              onClick={() => {
-                setCurrentPage("Portfolio");
-              }}
-            >
-              Portfolio
-            </li>
-            <li className={currentPage === 'Resume' ? "active": ""}
-              onClick={() => {
-                setCurrentPage("Resume");
-              }}
-            >
-              Resume
-            </li>
-          </ul>
+        <ul className="navbar">
+          <li
+            className={currentPage === "About Me" ? "active" : ""}
+            onClick={() => {
+              setCurrentPage("About Me");
+            }}
+          >
+            About Me
+          </li>
+          <li
+            className={currentPage === "Contact" ? "active" : ""}
+            onClick={() => {
+              setCurrentPage("Contact");
+            }}
+          >
+            Contact
+          </li>
+          <li
+            className={currentPage === "Portfolio" ? "active" : ""}
+            onClick={() => {
+              setCurrentPage("Portfolio");
+            }}
+          >
+            Portfolio
+          </li>
+          <li
+            className={currentPage === "Resume" ? "active" : ""}
+            onClick={() => {
+              setCurrentPage("Resume");
+            }}
+          >
+            Resume
+          </li>
+        </ul>
         {/* </div> */}
       </header>
       {getCurrentPage()}
+      <footer>
+      <h2>Contact Me</h2>
+        <ul>
+          <li>Cell: (760)685-7136</li>
+          <li>
+          Email:
+            <a href="demarcoalexandria5@gmail.com"
+              > demarcoalexandria5@gmail.com</a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/alex.demarco.16/">
+              Facebook</a>
+          </li>
+          <li>
+            <a href="www.linkedin.com/in/alex-demarco-002906b7">
+              LinkedIn</a>
+          </li>
+          <li>
+            <a href="https://github.com/allykatrocks">
+               Github</a>
+          </li>
+        </ul>
+      </footer>
     </div>
   );
 }
